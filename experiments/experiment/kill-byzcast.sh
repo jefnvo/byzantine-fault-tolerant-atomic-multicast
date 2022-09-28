7cf814e6088331c77273d5b0e4352b3f899dd926
Jefferson Oliveira <jeff_@MacBook-Air.local>
2022-09-28 14:43:13 -0300

    fix killing

--- experiments/experiment/kill-byzcast.sh
+++ experiments/experiment/kill-byzcast.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 server=$(ps aux | grep BatchServerGlobal | grep -v 'grep' | awk '{print $2}')
+localServer=$(ps aux | grep Server | grep -v 'grep' | awk '{print $2}')
 client=$(ps aux | grep Client | grep -v 'grep' | awk '{print $2}')
 
 if [[ -n $server ]]; then
@@ -8,6 +9,13 @@ if [[ -n $server ]]; then
     kill -9 $server
 else
     echo "server is dead"
+fi
+
+if [[ -n $localServer ]]; then
+    echo 'killing local server pid=$localServer'
+    kill -9 $localServer
+else
+    echo "local server is dead"
 fi
 
 if [[ -n $client ]]; then
