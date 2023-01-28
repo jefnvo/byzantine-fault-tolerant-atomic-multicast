ab81bb2d2e24ea3321ef585b165c0b79d60b9482
Jefferson Oliveira <jeff_@MacBook-Air.local>
2023-01-28 20:41:25 -0300

    fix clients

--- experiments/experiment/run-client.sh
+++ experiments/experiment/run-client.sh
@@ -48,4 +48,4 @@ if [ "$G_ID" = "" ]; then
 fi
 
 echo "Running $QTY clients in group $G_ID..."
-java -Xmx32G -cp '../../target/*:../../lib/*' ch.usi.inf.dslab.bftamcast.client.Client -i $RANDOM -g $G_ID -gc $GLOBALGROUPS $LOCALGROUPS -c 100 -p 100 $@ 
+java -Xmx32G -cp '../../target/*:../../lib/*' ch.usi.inf.dslab.bftamcast.client.Client -i $RANDOM -g $G_ID -gc $GLOBALGROUPS $LOCALGROUPS -c 8000 -p 100 $@ 
